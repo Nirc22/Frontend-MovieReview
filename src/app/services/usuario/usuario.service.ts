f028bc53d69fc0609629b9f76a7eccb1119a7df7
Nicolas Rodriguez <nircandsim@gmail.com>
2024-04-13 14:04:20 -0500

    Corrigiendo el _redirects y agregando en angular.json el src/_redirects

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { LoginRequest } from 'src/app/interfaces/login-requets';
-import { environment } from '../../../environments/environment';
+import { environment } from 'src/environments/environment';
 import { firstValueFrom, Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { MovieReview } from 'src/app/interfaces/movie-review';
